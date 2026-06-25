ef948023c49b75615219363122be732f533d5e88
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-25 20:18:09 +0530

    Array in classes and object

--- OOPs/ShopClassAndObject.cpp
+++ OOPs/ShopClassAndObject.cpp
@@ -17,9 +17,9 @@ public:
 
 void shop ::setData(void)
 {
-    cout << "Enter Iteam Id: ";
+    cout << "Enter Iteam Id of no : " << counter + 1 <<endl;
     cin >> IteamId[counter];
-    cout << "Enter Iteam Price: ";
+    cout << "Enter Iteam Price of no: " <<endl;
     cin >> IteamPrice[counter];
     counter++;
 }
@@ -30,13 +30,20 @@ void shop ::displayData(void)
     for (int i = 0; i < counter; i++)
     {
 
-        cout << "Iteam Id and Price is: " << IteamId[i] << " , " << IteamPrice[i];
+        cout << "Iteam Id and Price is: " << IteamId[i] << " , " << IteamPrice[i]  <<endl;
     }
 }
 
 int main()
 {
     system("cls");
+
+    shop dukaan;
+    dukaan.initcounter();
+    dukaan.setData();
+    dukaan.setData();
+    dukaan.setData();
+    dukaan.displayData();
 
     return 0;
 }
