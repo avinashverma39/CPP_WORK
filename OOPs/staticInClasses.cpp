e87f94208c9910bf3c2de4de3f1bc2524581123c
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-26 21:48:06 +0530

    Static class use in classes and object to retain the value

--- OOPs/staticInClasses.cpp
+++ OOPs/staticInClasses.cpp
@@ -9,13 +9,35 @@ class Employee
     static int count;
 
 public:
-    void setData(void);
-    void getData(void);
+    void setData(void)
+    {
+
+        cout << "Enter your Id: ";
+        cin >> Id;
+        count++;
+    }
+
+    void getData(void)
+    {
+
+        cout << "The Id of this employee  is " << Id << " and the  employee number is " << count << endl;
+    }
 };
 
+int Employee ::count = 100;
 int main()
 {
     system("cls");
+
+    Employee abhi, ram, rohan;
+    abhi.setData();
+    abhi.getData();
+
+    ram.setData();
+    ram.getData();
+
+    rohan.setData();
+    rohan.getData();
 
     return 0;
 }
