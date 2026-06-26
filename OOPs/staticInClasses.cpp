93d1a78f5bc38f67f04ca3c8e2dbed5d173936ed
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-26 21:33:50 +0530

    Static class use in object and class

---
+++ OOPs/staticInClasses.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee
+{
+
+    int Id;
+    static int count;
+
+public:
+    void setData(void);
+    void getData(void);
+};
+
+int main()
+{
+    system("cls");
+
+    return 0;
+}
